73724a6ae99e2b63b5facfd90e635cfc169ada90
Kimjh1214 <hzpotter1214@naver.com>
2021-06-23 10:48:10 +0900

    all

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <todoheader></todoheader>
   <todoinput @addTodo="addTodo"/>
   <todolist :todoItems="todoItems" @removeTodo="removeTodo"/>
-  <todofooter />
+  <todofooter @clearTodo="clearTodo"/>
 </template>
 
 <script>
@@ -45,6 +45,10 @@ export default {
     removeTodo(todoItem,index){
       localStorage.removeItem(todoItem)
       this.todoItems.splice(index,1)
+    },
+    clearTodo(){
+      localStorage.clear()
+      this.todoItems=[]
     }
   }
 }
--- src/components/todofooter.vue
+++ src/components/todofooter.vue
@@ -1,13 +1,29 @@
 <template>
-  footer
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" @click="$emit('clearTodo')">
+      Clear All
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name:'todofooter',
+  
 }
 </script>
 
 <style>
-
+  .clearAllContainer{
+    width: 8rem;
+    height: 50px;
+    line-height: 50px;
+    background: #fff;
+    border-radius: 5px;
+    margin: 0 auto;
+  }
+  .clearAllBtn{
+    color: #e20303;
+    display: block;
+  }
 </style>
